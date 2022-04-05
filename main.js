26f1ff937b43b071f65c6a01353e495a07b17091
boomafromboomiland <lubomira.kovacova@avon.com>
2022-04-05 18:33:10 +0200

    Druhá možnosť namiesto QuerySelector = getElementById

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let prveOvocie = document.querySelector('#moj-div').dataset.ovocie;
+let prveOvocie = document.getElementById('moj-div').dataset.ovocie;
 let druheOvocie = document.querySelector('#moj-druhy-div').dataset.ovocie;
 console.log('prveOvocie');
 console.log('druheOvocie');
